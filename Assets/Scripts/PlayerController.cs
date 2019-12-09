4cfe0529403995cb4ed6000e611d1b46288fcba6
amohayaee <14826uk@saeinstitute.edu>
2019-12-09 16:40:27 +0000

    Using Unity Assets to Test New Scene
    Also the editing environment is set to VS2019

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardSpeed = Input.GetAxis("Vertical");
+        
+
+        transform.Translate(Vector3.forward * Time.deltaTime *
+           speed * forwardSpeed);
+        transform.Rotate(Vector3.up, 
+            Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
