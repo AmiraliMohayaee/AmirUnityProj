8a8a698ba48a194624d087fed0f4133dfb0d0e71
amohayaee <14826uk@saeinstitute.edu>
2019-12-12 16:58:15 +0000

    12/12 Update

---
+++ Assets/Scripts/CameraRollBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRollBall : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position = player.transform.position;
+    }
+
+    // Updates after all other objects are updated first
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/RollBallComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollBallComponent : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Use for initializing physics components
+    void FixedUpdate()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHor, 0.0f, moveVert);
+
+        rb.AddForce(move * speed);
+    }
+}
